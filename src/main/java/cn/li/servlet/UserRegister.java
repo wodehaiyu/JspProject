ad82097335ba6d69b67fbdcb5e64ee7208e08c66
wodehaiyu <wodehaiyu@gmail.com>
2023-11-17 17:55:14 +0900

    ユーザー情報登録ページ の　servlet 新規

---
+++ src/main/java/cn/li/servlet/UserRegister.java
@@ -0,0 +1,25 @@
+package cn.li.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserRegister
+ */
+@WebServlet(description = "ユーザー情報登録ページ", urlPatterns = { "/userRegister" })
+public class UserRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().print("ユーザー情報登録ページ");
+	}
+
+}
